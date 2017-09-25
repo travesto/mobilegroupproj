b3c5b3e7c6c5e3f254b20fbbd38811b76a79a07a
Dcb426 <Dcb13a@acu.edu>
2017-09-25 17:12:36 -0500

    camera functonaillty

--- app/src/main/java/teammint/classroster/MainActivity.java
+++ app/src/main/java/teammint/classroster/MainActivity.java
@@ -1,18 +1,32 @@
 package teammint.classroster;
 
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TabHost;
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-import android.widget.TabHost;
 
 
 public class MainActivity extends AppCompatActivity {
     TabHost tabHost;
+    Button btn;
+    private static  final int CAMERA_REQUEST = 123;
+    ImageView b;
+    Drawable D;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.takePic);
+        b = (ImageView) findViewById(R.id.Frame);
 
         TabHost host = (TabHost)findViewById(R.id.tabHost);
         host.setup();
@@ -30,12 +44,32 @@ public class MainActivity extends AppCompatActivity {
         host.addTab(spec);
 
         Spinner dropdown = (Spinner)findViewById(R.id.gender);
-//create a list of items for the spinner.
+        //create a list of items for the spinner.
         String[] items = new String[]{"Male", "Female","Other"};
-//create an adapter to describe how the items are displayed, adapters are used in several places in android.
-//There are multiple variations of this, but this is the basic variant.
+        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
+        //There are multiple variations of this, but this is the basic variant.
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
-//set the spinners adapter to the previously created one.
+        //set the spinners adapter to the previously created one.
         dropdown.setAdapter(adapter);
+    }
+    //Capture camera intent
+    public void btnClick(View v)
+    {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, CAMERA_REQUEST);
+    }
+    //Clear picture data
+    public void clear(View v)
+    {
+        b.setBackground(null);
+    }
+    //Store camera data in bitmap and set frame
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK)
+        {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            b.setImageBitmap(photo);
+        }
     }
 }
