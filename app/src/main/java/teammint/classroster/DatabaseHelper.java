d4e7cdcbc7a7066763510530f2f0c50ac5f91dbc
DESKTOP-KP8ENKK\natta <nxw13a@acu.edu>
2017-10-06 13:07:25 -0500

    my stuff

--- app/src/main/java/teammint/classroster/DatabaseHelper.java
+++ app/src/main/java/teammint/classroster/DatabaseHelper.java
@@ -21,7 +21,7 @@ import android.util.Log;
 
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-
+    
     private static final String TAG = "DatabaseHelper";
     public static final String DATABASE_NAME = "SQLiteExample.db";
     private static final int DATABASE_VERSION = 1;
