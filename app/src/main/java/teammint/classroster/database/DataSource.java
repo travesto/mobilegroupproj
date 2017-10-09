0d7ebf6c28afa0fbade186512afe7ef5823beef5
Dcb426 <Dcb13a@acu.edu>
2017-10-09 13:17:15 -0500

    Working Database

---
+++ app/src/main/java/teammint/classroster/database/DataSource.java
@@ -0,0 +1,82 @@
+package teammint.classroster.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import teammint.classroster.model.DataStudent;
+
+/**
+ * Created by Darius Bell on 10/9/2017.
+ */
+
+public class DataSource {
+
+    private Context mContext;
+    private SQLiteDatabase mdatabase;
+    private SQLiteOpenHelper mdbHelper;
+
+    public DataSource(Context context) {
+        this.mContext = context;
+        mdbHelper = new DBhelper(mContext);
+        mdatabase = mdbHelper.getWritableDatabase();
+    }
+    public void open(){
+        mdatabase = mdbHelper.getWritableDatabase();
+    }
+    public void close(){
+    mdbHelper.close();
+
+    }
+    public DataStudent createStudent(DataStudent student){
+        ContentValues values = student.toValues();
+        mdatabase.insert(StudentsTable.TABLE_STUDENT,null,values);
+        return student;
+    }
+    public long getdataItemCount() {
+        return DatabaseUtils.queryNumEntries(mdatabase, StudentsTable.TABLE_STUDENT);
+    }
+    public long getdataSimilarCount(String A) {
+        String x = "SELECT " + StudentsTable.COLUMN_NOTES + " FROM " + StudentsTable.TABLE_STUDENT + "WHERE " + StudentsTable.COLUMN_NOTES + " = " + A;
+        return DatabaseUtils.queryNumEntries(mdatabase,StudentsTable.TABLE_STUDENT, x);
+    }
+    public String databaseToString() {
+        String dbString = "";
+        String query = "Select * From " + StudentsTable.TABLE_STUDENT;
+        Cursor record = mdatabase.rawQuery(query,null);
+        record.moveToFirst();
+
+        while(!record.isAfterLast())
+        {
+            dbString += record.getString(record.getColumnIndex("Major"));
+            //dbString += "\n";
+            record.moveToNext();
+        }
+        record.close();
+        return dbString;
+    }
+    public List<DataStudent> getAll(){
+        List<DataStudent> dataStudents = new ArrayList<>();
+        Cursor cursor = mdatabase.query(StudentsTable.TABLE_STUDENT, StudentsTable.ALL_COL,
+                null,null,null,null,null);
+        while(cursor.moveToNext()){
+            DataStudent student = new DataStudent();
+            student.setID(cursor.getString(cursor.getColumnIndex(StudentsTable.COLUMN_ID)));
+            student.setFName(cursor.getString(cursor.getColumnIndex(StudentsTable.COLUMN_FNAME)));
+            student.setLName(cursor.getString(cursor.getColumnIndex(StudentsTable.COLUMN_LNAME)));
+            student.setMajor(cursor.getString(cursor.getColumnIndex(StudentsTable.COLUMN_MAJOR)));
+            student.setHome(cursor.getString(cursor.getColumnIndex(StudentsTable.COLUMN_LOCATION)));
+            student.setGender(cursor.getString(cursor.getColumnIndex(StudentsTable.COLUMN_GENDER)));
+            student.setNotes(cursor.getString(cursor.getColumnIndex(StudentsTable.COLUMN_NOTES)));
+            student.setImage(cursor.getString(cursor.getColumnIndex(StudentsTable.COLUMN_IMAGE)));
+            dataStudents.add(student);
+        }
+        return dataStudents;
+    }
+}
---
+++ app/src/main/java/teammint/classroster/model/DataStudent.java
@@ -0,0 +1,111 @@
+package teammint.classroster.model;
+
+import android.content.ContentValues;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.io.ByteArrayOutputStream;
+
+import teammint.classroster.database.StudentsTable;
+
+/**
+ * Created by Darius Bell on 10/9/2017.
+ */
+
+public class DataStudent {
+
+    private String studentid;
+    private String studentFName;
+    private String studentLName;
+    private String major;
+    private String location;
+    private String gender;
+    private String notes;
+    private String image;
+    private Byte[] imageStore;
+
+        public DataStudent(){
+
+        }
+
+        private static byte[] getBytes(Bitmap bitmap) {
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 0, stream);
+            return stream.toByteArray();
+        }
+        private static Bitmap getImage(byte[] image) {
+            return BitmapFactory.decodeByteArray(image, 0, image.length);
+        }
+
+        //Setters
+        public void setFName(String n) {
+            this.studentFName = n;
+        }
+        public void setLName(String n) {
+            this.studentLName = n;
+        }
+        public void setMajor(String m) {
+            this.major = m;
+        }
+        public void setHome(String h) {
+            this.location = h;
+        }
+        public void setNotes(String o) {
+            this.notes = o;
+        }
+        public void setImage(String b) {
+            this.image = (b);
+        }
+        public void setGender(String g) {
+            this.gender = g;
+        }
+        public void setID(String a) {
+            this.studentid = a;
+        }
+        //public void setImage(Bitmap c) {this.image=c;}
+
+        //Getters
+        public String getID() {
+            return this.studentid;
+        }
+        public String getFName() {
+            return this.studentFName;
+        }
+        public String getLName() {
+            return this.studentLName;
+        }
+        public String getMajor() {
+            return this.major;
+        }
+        public String getHome() {
+            return this.location;
+        }
+        public String getNotes() {
+            return this.notes;
+        }
+        public String getImage() {
+            return this.image;
+        }
+        //public byte[] getImage() {
+          //  return getBytes(this.image);
+        //}
+        public String getGender() {
+            return this.gender;
+        }
+
+        public ContentValues toValues()
+        {
+            ContentValues val = new ContentValues(8);
+            val.put(StudentsTable.COLUMN_ID,studentid);
+            val.put(StudentsTable.COLUMN_FNAME, studentFName);
+            val.put(StudentsTable.COLUMN_LNAME, studentLName);
+            val.put(StudentsTable.COLUMN_MAJOR, major);
+            val.put(StudentsTable.COLUMN_LOCATION, location);
+            val.put(StudentsTable.COLUMN_GENDER, gender);
+            val.put(StudentsTable.COLUMN_NOTES, notes);
+            val.put(StudentsTable.COLUMN_IMAGE,image);
+            return val;
+        }
+
+
+}
